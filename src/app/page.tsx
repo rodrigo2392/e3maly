87168b2eefa8b1d70196aae5aebc8de1e2159052
Rodrigo Mendez <itic.rodrigomg@gmail.com>
2023-08-18 21:10:57 -0600

    footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import NavBar from "@/components/NavBar"
 import Header from "@/components/Header";
 import Card from "@/components/Card";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -11,9 +12,10 @@ export default function Home() {
           <Header />
         </div>
       </div>
-      <div className="flex justify-center green-gradient pb-96">
+      <div className="flex justify-center green-gradient">
         <div className="max-w-6xl w-full">
           <Card />
+          <Footer />
         </div>
       </div>
     </main>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,11 @@ import Image from 'next/image'
 
 export default function Header() {
 return <>
-<div className='w-full justify-center flex relative pb-0 px-4'>
+<div className='w-full justify-center flex relative mt-10 px-4 pb-10'>
             <Image
-              src="/card.png"
+              src="/footer.png"
               alt="Firma"
-              className="w-full lg:w-4/5 h-auto z-10"
+              className="w-full lg:w-1/2 h-auto z-10"
               width={1500}
               height={1500}
               priority
